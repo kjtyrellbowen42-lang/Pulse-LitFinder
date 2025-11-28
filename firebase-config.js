ed410be52c474da9e58245f55fbbfee1a18a4d9d
kjtyrellbowen42-lang <kjtyrellbowen42@gmail.com>
2025-11-27 22:20:16 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyAMMN1No3naog7jaGNqtybtsBg5boU0FVU",
+authDomain: "pulse-litfinder.firebaseapp.com",
+projectId: "pulse-litfinder",
+storageBucket: "pulse-litfinder.firebasestorage.app",
+messagingSenderId: "9560830674762",
+appId: "1:9560830674762:web:d6aa98ae07ccf1609183b3",
+measurementId: "G-9G96JZG67B"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
