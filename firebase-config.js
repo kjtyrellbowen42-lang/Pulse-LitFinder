7c2a36ccb16923f478d36bea38d2b3015e6e0d3f
kjtyrellbowen42-lang <kjtyrellbowen42@gmail.com>
2025-11-27 22:38:43 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-apiKey: "AIzaSyAMMN1No3naog7jaGNqtybtsBg5boU0FVU",
+window.firebaseConfig = {
+apiKey: "AIzaSyMMNNNo3naog7jaGNqtvbtsBq5b0U8FVU",
 authDomain: "pulse-litfinder.firebaseapp.com",
 projectId: "pulse-litfinder",
-storageBucket: "pulse-litfinder.firebasestorage.app",
+storageBucket: "pulse-litfinder.appspot.com",
 messagingSenderId: "9560830674762",
 appId: "1:9560830674762:web:d6aa98ae07ccf1609183b3",
 measurementId: "G-9G96JZG67B"
 };
 
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(window.firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
